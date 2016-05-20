e540047d36192e04f473434ff975488c890bf5a0
Pinselohrkater <cougarism@gmail.com>
2016-05-20 14:09:17 +0200

    Proper wiring up of translations / resw template

---
+++ Eurofurence.Companion/Translations.cs
@@ -0,0 +1,27 @@
+﻿
+using Windows.ApplicationModel.Resources; 
+
+namespace Eurofurence.Companion 
+{
+    public static class Translations 
+    {
+        private static readonly ResourceLoader _resourceLoader; 
+
+        static Translations() 
+        {
+            _resourceLoader = ResourceLoader.GetForViewIndependentUse("Translations");
+        }
+
+        public static string EventSchedule_Title 
+			=> _resourceLoader.GetString("EventSchedule_Title"); 
+
+        public static string Dealers_Title 
+			=> _resourceLoader.GetString("Dealers_Title"); 
+
+        public static string ConventionInformation_Title 
+			=> _resourceLoader.GetString("ConventionInformation_Title"); 
+
+        public static string Info_Title 
+			=> _resourceLoader.GetString("Info_Title"); 
+    }
+}
