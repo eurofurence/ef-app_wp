3ba2c251b5e78e659bb5c33addc664392c32e303
Pinselohrkater <cougarism@gmail.com>
2016-06-06 23:29:27 +0200

    Adding Page State to Dealers List (fixes #3), first work on detail page #5

--- Eurofurence.Companion/Views/DealerListPage.xaml.cs
+++ Eurofurence.Companion/Views/DealerListPage.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using Eurofurence.Companion.Common;
+using System;
+using System.Linq;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Navigation;
 
@@ -7,6 +9,8 @@ namespace Eurofurence.Companion.Views
 
     public sealed partial class DealerListPage : Page, IPageProperties
     {
+        private const string STATE_SCROLLVIEWER_OFFSET_Y = "scrollViewer.OffsetY";
+
         public DealerListPage()
         {
             InitializeComponent();
@@ -23,13 +27,23 @@ namespace Eurofurence.Companion.Views
         public ObservableDictionary DefaultViewModel { get; } = new ObservableDictionary();
 
 
+        private ScrollViewer GetListViewScrollViewer()
+        {
+            E_ListView_Dealers.UpdateLayout();
+            return E_ListView_Dealers.Descendents().OfType<ScrollViewer>().FirstOrDefault();
+        }
+
+
         private void NavigationHelper_LoadState(object sender, LoadStateEventArgs e)
         {
+            var verticalOffset = Convert.ToDouble(e.PageState?[STATE_SCROLLVIEWER_OFFSET_Y] ?? 0);
+            GetListViewScrollViewer().ChangeView(null, verticalOffset, null);
         }
 
 
         private void NavigationHelper_SaveState(object sender, SaveStateEventArgs e)
         {
+            e.PageState[STATE_SCROLLVIEWER_OFFSET_Y] = GetListViewScrollViewer().VerticalOffset;
         }
 
         public const string PAGE_ICON = "\uE13D";
