006e37cbe6e9766db764fc18afbffda520df0ec7
Pinselohrkater <cougarism@gmail.com>
2016-07-12 16:30:12 +0200

    Adding navigation support for Map Markers (->Dealers)

--- Eurofurence.Companion/Views/Controls/MapViewerControl.xaml.cs
+++ Eurofurence.Companion/Views/Controls/MapViewerControl.xaml.cs
@@ -1,7 +1,13 @@
-﻿using Windows.UI;
+﻿using System.Linq;
+using Windows.UI;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
+using Eurofurence.Companion.DependencyResolution;
+using Eurofurence.Companion.ViewModel.Abstractions;
+using Eurofurence.Companion.ViewModel.Local.Entity;
+using Ninject;
 
 namespace Eurofurence.Companion.Views.Controls
 {
@@ -23,6 +29,26 @@ namespace Eurofurence.Companion.Views.Controls
             MarkerFill = new SolidColorBrush(Color.FromArgb(70, 255, 0, 0));
             MarkerStroke = new SolidColorBrush(Color.FromArgb(255, 255, 0, 0));
             MarkerScale = 1d;
+        }
+
+        private void MarkerTapped(object sender, TappedRoutedEventArgs e)
+        {
+            var marker = ((sender as FrameworkElement)?.DataContext as MapEntryViewModel);
+            if (marker == null) return;
+
+            switch (marker.Entity.MarkerType)
+            {
+                case "Dealer":
+
+                    var context = KernelResolver.Current.Get<IDealersViewModelContext>();
+                    var dealer = context.Dealers.SingleOrDefault(a => a.Entity.Id == marker.Entity.TargetId);
+
+                    if (dealer == null) return;
+                    ViewModelLocator.Current.NavigationViewModel.NavigateToDealerDetailPage.Execute(dealer);
+
+                    break;
+            }
+
         }
     }
 }
