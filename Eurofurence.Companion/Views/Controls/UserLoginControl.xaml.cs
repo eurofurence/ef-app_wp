86ce228c265beff4236e13684bc270fc6b6b0b25
Luchs <luchs@pinselohr.ch>
2017-07-07 10:32:44 +0200

    Order/Usability improvements on Login Form

--- Eurofurence.Companion/Views/Controls/UserLoginControl.xaml.cs
+++ Eurofurence.Companion/Views/Controls/UserLoginControl.xaml.cs
@@ -14,5 +14,18 @@ namespace Eurofurence.Companion.Views.Controls
                 E_Button_Login.Focus(FocusState.Keyboard);
             };
         }
+
+        private void E_PasswordBox_KeyUp(object sender, Windows.UI.Xaml.Input.KeyRoutedEventArgs e)
+        {
+            if (e.Key == Windows.System.VirtualKey.Enter)
+                E_Button_Login.Command.Execute(null);
+        }
+
+        private void E_TextBox_Nickname_KeyUp(object sender, Windows.UI.Xaml.Input.KeyRoutedEventArgs e)
+        {
+            if (e.Key == Windows.System.VirtualKey.Enter)
+                E_PasswordBox.Focus(FocusState.Keyboard);
+
+        }
     }
 }
