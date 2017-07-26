89baa48158474813797584b977e8a49a2db6444d
Luchs <luchs@pinselohr.ch>
2017-07-26 11:19:58 +0200

    Fixing Con Start Debug + Mainpage Scroll Jitter

--- Eurofurence.Companion/Views/Controls/DebugControl.xaml.cs
+++ Eurofurence.Companion/Views/Controls/DebugControl.xaml.cs
@@ -39,7 +39,7 @@ namespace Eurofurence.Companion.Views.Controls
 
         private void SetTime_ConStart(object sender, TappedRoutedEventArgs e)
         {
-            SetConLocalTime(2016, 08, 16, 9, 00, 00);
+            SetConLocalTime(2017, 08, 16, 9, 0, 0);
         }
 
         private void SetTime_AddDay(object sender, TappedRoutedEventArgs e)
