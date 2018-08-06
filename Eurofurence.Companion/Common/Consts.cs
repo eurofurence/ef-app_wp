54745ab371ccf9c8021ed6e6332c6ad520c07b8a
Luchs <luchs@pinselohr.ch>
2018-08-06 10:36:03 +0200

    Applying live endpoint

--- Eurofurence.Companion/Common/Consts.cs
+++ Eurofurence.Companion/Common/Consts.cs
@@ -2,7 +2,7 @@
 {
     public class Consts
     {
-        public const string WEB_API_ENDPOINT_URL = "https://app.eurofurence.org:40000/api/v2";
+        public const string WEB_API_ENDPOINT_URL = "https://app.eurofurence.org/api/v2";
         //public const string WEB_API_ENDPOINT_URL = "http://localhost:30001/api/v2";
     }
 }
