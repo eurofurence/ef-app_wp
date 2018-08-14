7bced6ba628569064af72737989fb2876d698cd7
Pinselohrkater <cougarism@gmail.com>
2018-08-14 11:24:40 +0200

    Login Dialog change, bump to 2.2.0.4

--- Eurofurence.Companion/App.xaml.cs
+++ Eurofurence.Companion/App.xaml.cs
@@ -294,9 +294,8 @@ namespace Eurofurence.Companion
                 switch (applicationSettingsContext.LastPackageVersionRunning)
                 {
                     // Accepted previous versions that won't require a reset
-                    /*case "2.2.0.0":
-                    case "2.2.0.1":
-                        break;*/
+                    case "2.2.0.3":
+                        break;
                     default:
                         var contextManager = KernelResolver.Current.Get<ContextManager>();
                         await contextManager.ClearAll();
